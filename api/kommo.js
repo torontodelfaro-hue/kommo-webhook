58dfa7f661d912fe51bd49d30b1df8e38b9a0077
torontodelfaro-hue <torontodelfaro@gmail.com>
2026-02-14 07:22:35 -0300

    Update kommo.js

--- api/kommo.js
+++ api/kommo.js
@@ -1,35 +1,26 @@
-const crypto = require('crypto');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
-    let phone = req.body.contacts?.[0]?.phone || "";
-phone = phone.replace(/\D/g, "");
+    const phoneRaw = req.body?.contacts?.[0]?.phone || "";
 
-    phone = phone.replace(/\D/g, '');
-
-    if (!phone) {
+    if (!phoneRaw) {
       return res.status(200).json({ message: "No phone provided" });
     }
 
-    const hashedPhone = crypto
-      .createHash('sha256')
-      .update(phone)
-      .digest('hex');
+    const phone = phoneRaw.replace(/\D/g, "");
 
     const response = await fetch(
-      'https://graph.facebook.com/v19.0/1043319464664254/events?1043319464664254=EAARmicyII3gBQrsZCGePXxusolNKcv2jwzE52CHy7JwTsarB1sENIvpZC77crC6ntXZBwXQE3MgMetYWIX1cVoXsbP7mnHDSXeVQCBOFDBRTZAvK59TiiDDc3g7YzVdSIMoo6cehctS6WmGGtyDlQPNTR0s39yAzy5CPNJ3kBRleEBVHCQqZCT61BhQ8we31BUgZDZD',
+      "https://graph.facebook.com/v19.0/1043319464664254/events?access_token=EAARmicyII3gBQrsZCGePXxusolNKcv2jwzE52CHy7JwTsarB1sENIvpZC77crC6ntXZBwXQE3MgMetYWIX1cVoXsbP7mnHDSXeVQCBOFDBRTZAvK59TiiDDc3g7YzVdSIMoo6cehctS6WmGGtyDlQPNTR0s39yAzy5CPNJ3kBRleEBVHCQqZCT61BhQ8we31BUgZDZD",
       {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          test_event_code: "TEST49976",
           data: [
             {
               event_name: "Purchase",
               event_time: Math.floor(Date.now() / 1000),
               action_source: "system_generated",
               user_data: {
-                ph: [hashedPhone]
+                ph: [phone]
               }
             }
           ]
@@ -37,13 +28,13 @@ phone = phone.replace(/\D/g, "");
       }
     );
 
-    const fbData = await response.json();
-    console.log("Facebook response:", fbData);
+    const data = await response.json();
+    console.log("META RESPONSE:", data);
 
-    return res.status(200).json({ success: true, fbData });
+    return res.status(200).json({ success: true, meta: data });
 
   } catch (error) {
-    console.error(error);
+    console.error("ERROR:", error);
     return res.status(500).json({ error: error.message });
   }
-};
+}
